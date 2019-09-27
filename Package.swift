2ff0e18437a40aba71dab94c886a57b459b012dd
Creator William <>
2019-09-27 16:07:35 +0800

    调整为静态库

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NetworkKit",
-            type: .dynamic,
+            type: .static,
             targets: ["NetworkKit"]),
     ],
     dependencies: [
